4859fbb56ed443926c65aeba3e180a37ae487fda
mostafasaqly <mostafasaqly1@gmail.com>
2024-02-16 20:02:35 +0200

    first commit

---
+++ src/app/Compoenets/todo/todo.component.ts
@@ -0,0 +1,92 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import { Task } from '../../Models/task';
+import {MatButtonModule} from '@angular/material/button';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+  CdkDrag,
+  CdkDropList,
+} from '@angular/cdk/drag-drop';
+@Component({
+  selector: 'app-todo',
+  standalone: true,
+  imports: [
+    MatCardModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    CdkDropList, CdkDrag
+  ],
+  templateUrl: './todo.component.html',
+  styleUrl: './todo.component.scss'
+})
+export class TodoComponent implements OnInit {
+todoForm! : FormGroup;
+tasks: Task[] = [];
+inprogress:Task[] = [];
+done:Task[] = [];
+
+isEditEnabled: boolean = false;
+updatedIndex!:any;
+constructor(private fb:FormBuilder){}
+  ngOnInit(): void {
+    this.todoForm = this.fb.group({
+      item : ['', [Validators.required, Validators.minLength(3)]]
+    });
+  }
+
+  addTask()
+  {
+    this.tasks.push({
+      Title : this.todoForm.value.item,
+      Completed:false
+    });
+    this.todoForm.reset();
+  }
+  updateTask()
+  {
+    this.tasks[this.updatedIndex].Title = this.todoForm.value.item;
+    this.tasks[this.updatedIndex].Completed = false;
+    this.todoForm.reset();
+    this.updatedIndex = undefined;
+    this.isEditEnabled = false;
+  }
+  deleteTask(taskId:number){
+    this.tasks.splice(taskId,1);
+  }
+  deleteInprogressTask(taskId:number){
+    this.inprogress.splice(taskId,1);
+  }
+  deleteDoneTask(taskId:number){
+    this.done.splice(taskId,1);
+  }
+
+  onEditTask(task:Task,TaskId:number)
+  {
+    this.todoForm.controls['item'].setValue(task.Title);
+    this.updatedIndex = TaskId;
+    this.isEditEnabled = true;
+  }
+
+  drop(event: CdkDragDrop<Task[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { TodoComponent } from './Compoenets/todo/todo.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo:'/todo', pathMatch:'full'},
+  {path:"todo", component:TodoComponent, title: "Todo"}
+];
